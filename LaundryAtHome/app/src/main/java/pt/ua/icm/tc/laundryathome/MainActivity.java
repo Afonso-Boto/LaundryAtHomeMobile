858b2bebf42ec65d71ce4045964f916eb78da484
Tomás Candeias <tomascandeias@ua.pt>
2022-06-20 16:40:48 +0100

    login/register ui

--- LaundryAtHome/app/src/main/java/pt/ua/icm/tc/laundryathome/MainActivity.java
+++ LaundryAtHome/app/src/main/java/pt/ua/icm/tc/laundryathome/MainActivity.java
@@ -1,5 +1,6 @@
 package pt.ua.icm.tc.laundryathome;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
         setContentView(R.layout.activity_main);
     }
 }
